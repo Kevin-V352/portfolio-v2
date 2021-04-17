03e76e0dbf72b50a080afefd982117914877840a
Kevin <kevinvega2070@gmail.com>
2021-04-17 12:07:50 -0300

    Introduction - added additional information

--- src/components/homepage/introduction/introduction.tsx
+++ src/components/homepage/introduction/introduction.tsx
@@ -4,13 +4,22 @@ import * as S from './introduction-elements';
 
 const Introduction: FC = (): JSX.Element => (
   <S.Container>
-    <S.Title
-      data-aos='fade-down'
-      data-aos-easing='fade-down'
-      data-aos-delay='100'
-    >
+    <S.Title data-aos='fade-down' data-aos-delay='100'>
       Introduccion
     </S.Title>
+    <S.AnimationLottie
+      options={{
+        loop: false,
+        animationData: require('../../../shared/json/init-animation.json'),
+      }}
+    />
+    <S.Description data-aos='zoom-in' data-aos-delay='100'>
+      Bienvenido a mi portfolio!
+      <br />
+      Soy Kevin Vega, un desarrollador Full-Stack.
+      <br />
+      Exploremos un poco este perfil 🤖
+    </S.Description>
   </S.Container>
 );
 
