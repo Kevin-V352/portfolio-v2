e70be12d3bfa74e1bfed54e04c349735cb005f4e
Kevin <kevinvega2070@gmail.com>
2021-04-17 12:03:56 -0300

    Context - modified context schema

--- src/context/context-state.tsx
+++ src/context/context-state.tsx
@@ -1,25 +1,16 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { Coordinates } from '../shared/interfaces/global-interfaces';
 import { Provider } from './contexts';
 
 const Contex: FC = ({ children }): JSX.Element => {
-  const [coordinatesY, setCoordinatesY] = useState<Coordinates>({
+  let coordinatesY: Coordinates = {
     introductionY: 0,
     stackY: 0,
     projectsY: 0,
     contactY: 0,
-  });
+  };
 
-  return (
-    <Provider
-      value={{
-        coordinatesY,
-        setCoordinatesY,
-      }}
-    >
-      {children}
-    </Provider>
-  );
+  return <Provider value={{ coordinatesY }}>{children}</Provider>;
 };
 
 export default Contex;
