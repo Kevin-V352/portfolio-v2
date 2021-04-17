70e88c05f9f135802d767e5c63b8b163c095ce78
Kevin <kevinvega2070@gmail.com>
2021-04-17 12:16:06 -0300

    Introduction - added stylized components - modified style parameters

--- src/components/homepage/introduction/introduction-elements.ts
+++ src/components/homepage/introduction/introduction-elements.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
+import Lottie from 'react-lottie';
 
 export const Container = styled.section`
   height: 100vh;
   width: 100%;
   display: grid;
-  grid-template-rows: min-content auto;
   grid-template-columns: 90%;
+  grid-template-rows: min-content repeat(
+      2,
+      calc((100% - (var(--big-font-size) + 1.4vh)) / 2)
+    );
   justify-content: center;
+  justify-items: center;
+  align-items: center;
   margin-bottom: 5vh;
 `;
 
@@ -18,4 +24,16 @@ export const Title = styled.h2`
   text-align: center;
   margin: 0;
   font-size: var(--big-font-size);
+`;
+
+export const AnimationLottie = styled(Lottie).attrs({
+  height: '130%',
+})``;
+
+export const Description = styled.span`
+  color: var(--gray);
+  font-size: var(--big-font-size);
+  width: max-content;
+  text-align: center;
+  align-self: flex-start;
 `;
