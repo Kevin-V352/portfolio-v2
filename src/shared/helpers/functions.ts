f771ef8c2c1c60a6c18219b41c74ff9d4918458c
Kevin <kevinvega2070@gmail.com>
2021-04-17 12:02:37 -0300

    Helpers functions - minimal fixes

--- src/shared/helpers/functions.ts
+++ src/shared/helpers/functions.ts
@@ -1,4 +1,4 @@
 //Get the relative position of an element with respect to the document
 export const getCoordinate = (position: number | undefined): number => {
-  return position ? position + window.scrollY : 0;
+  return position ? Math.round(position + window.scrollY) : 0;
 };
